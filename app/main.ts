b9230fd28273b1b5e5048ce2dc4ffa935201fecd
Yehor Orlov <egor7orlov@proton.me>
2024-06-01 18:01:27 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
+    connection.on("data", (data: Buffer) => {
+        connection.end("+PONG\r\n");
+    });
 });
 
 server.listen(6379, "127.0.0.1");
